7b8c211c356f5da3688db2f7005bcabd8f229b60
hana-rs <hanahanabi87@icloud.com>
2024-12-27 21:46:28 +0900

    App.jsxをList.jsxに変更

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import List from './List.jsx'
 import Register from './Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Register />
-    <App />
+    <List />
   </StrictMode>,
 )
 
