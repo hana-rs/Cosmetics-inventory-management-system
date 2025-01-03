f247bd1a473a3201d558d51e4b03edb0f9217d50
hana-rs <hanahanabi87@icloud.com>
2025-01-03 23:26:46 +0900

    商品を追加ボタンを非表示

--- client/src/List.jsx
+++ client/src/List.jsx
@@ -95,7 +95,7 @@ function App() {
           {items.map((item) => (
             <li key={item.id}>
               {item.item_name} {item.item_memo} 在庫数:{item.item_count}
-              <button onClick={() => add_item_count(item.id)}>在庫を追加</button>
+              {/* <button onClick={() => add_item_count(item.id)}>在庫を追加</button> */}
               {item.item_opened !== 0 && (
                <> 開封日：{item.item_opened_date} 期限日：{item.item_limited_date} <button onClick={() => dec_item_count(item.id)}>使い切った</button></>
               )}
